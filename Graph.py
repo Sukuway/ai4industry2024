7f66dcb14497c35bb4dbb77dedb8e6a445385f3e
“Leo-Paul <lpmaziere@bordeaux-inp.fr>
2024-01-24 13:57:28 +0100

    graph

--- Graph.py
+++ Graph.py
@@ -2,25 +2,68 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import networkx as nx 
+from itertools import combinations
 
 
-nodes = ['A', 'B', 'C']
-edges = [('A', 'C'), ('A', 'B'), ('B', 'C')]
+abstract_nodes = ['A', 'B', 'C']
+nodes = [1, 2, 3]
+edges = [('A', 1), ('B', 2), ('B', 3)]
 
-def print_sub_graph(hypernodes, nodes, edges):
-    keep_edges = [edge for edge in edges if (edge[0] in hypernodes) or (edge[1] in hypernodes)]
-    keep
+
+def make_graph(nodes, edges):
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    return g
+
+
+
+def sub_graph(abstract_nodes, nodes, edges):
+    keep_edges = [edge for edge in edges if (edge[0] in abstract_nodes)]
+    keep_nodes = [edge[1] for edge in keep_edges]+abstract_nodes
     return keep_nodes, keep_edges
 
+def test_connected(nodes, edges):
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    return nx.is_connected(g)
+
+def fully_connected(abstract_nodes, nodes, edges):
+    unconnected = []
+    connected = []
+    for i,j in combinations(abstract_nodes, 2):
+        keep_nodes, keep_edges = sub_graph([i,j], nodes, edges)
+        if test_connected(keep_nodes, keep_edges):
+            connected.append((i,j))
+        else :
+            unconnected.append((i,j))
+    return unconnected, connected
 
 
-G = nx.Graph()
-G.add_nodes_from(nodes)
-G.add_edges_from(edges)
+
+def test():
+
+    commu = nx.community.louvain_communities(make_graph(nodes+abstract_nodes, edges))
+    print(commu)
+    unco, co = fully_connected(abstract_nodes, nodes, edges)
+    print(len(unco), len(co))
 
 
-pos = nx.spring_layout(G)
+    # sub_nodes, sub_edges = sub_graph(abstract_nodes, nodes, edges)
 
-nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue', font_size=10)
+    # G = nx.Graph()
 
-plt.show()
+    # G.add_nodes_from(sub_nodes)
+    # G.add_edges_from(sub_edges)
+
+    # print(nx.is_connected(G))
+
+    # pos = nx.spring_layout(G)
+
+    # nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=100, node_color='skyblue', font_size=10)
+
+    # plt.show()
+    return
+
+test()
