35dc87dbe8ed81027644e09a914aa793ba715028
“Leo-Paul <lpmaziere@bordeaux-inp.fr>
2024-01-25 10:20:13 +0100

    class update

--- Graph.py
+++ Graph.py
@@ -7,21 +7,21 @@ class Graph():
         self.N = N
         self.folder = folder
 
-    def init_nodes_and_edges(self):
-        stats = Stats(N=self.N, folder=self.folder)
+    def init_nodes_and_edges(self, N=3):
+        self.stats = Stats(N=self.N, folder=self.folder)
 
-        abstract_nodes = stats.unique_key_value_pairs()
-        nodes = stats.nodes()
+        abstract_nodes = self.stats.unique_key_value_pairs()
+        nodes = self.stats.nodes()
 
-        self.idx_to_xy, self.xy_to_idx = stats.mapping_dictionaries(nodes)
+        self.idx_to_xy, self.xy_to_idx = self.stats.mapping_dictionaries(nodes)
 
-        edges = stats.edges()
+        edges = self.stats.edges()
 
         self.abstract_nodes: list = list(abstract_nodes)
         self.nodes: list = [self.xy_to_idx[node] for node in nodes]
 
-        self.edges: list = stats.edges_formatting(edges, self.xy_to_idx)
-        self.distance_edges: list = stats.distance_edges(self.nodes, N=3)
+        self.edges: list = self.stats.edges_formatting(edges, self.xy_to_idx)
+        self.distance_edges: list = self.stats.distance_edges(self.nodes, N)
 
     def get_all_nodes(self) -> list:
         return self.nodes + self.abstract_nodes
@@ -70,17 +70,19 @@ class Graph():
         nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=500, node_color=color, font_size=10)
         plt.show()
 
-    """def get_neighbors_at_depth(adjacency_list, features, depth):
+    def get_neighbors_at_depth(self, adjacency_list, features, depth):
         visited = set()
         saved = set()
 
         def dfs(nodes, features):
-
+            print(nodes)
             if len(nodes) > depth:
                 return
             
             for neighbor in adjacency_list[nodes[-1]]:
-                if neighbor not in visited:
+                if (neighbor not in features) and (neighbor in self.abstract_nodes):
+                    return
+                elif neighbor not in visited:
                     visited.add(neighbor)
                     if  (neighbor in features) and (neighbor != nodes[0]):
                         for k in nodes:
@@ -93,20 +95,25 @@ class Graph():
     
         return saved
 
-    def get_neighbors_at_distance(adjacency_list, features, dist):
+
+    def get_neighbors_at_distance(self, adjacency_list, features, dist):
         visited = set()
         saved = set()
 
         def dfs(nodes, features):
             total_dist = 0
             for i in range(1,len(nodes)-1):
-                total_dist+=distance(i,i+1)
+                coord1 = self.idx_to_xy(nodes[i])
+                coord2 = self.idx_to_xy(nodes[i+1])
+                total_dist+=self.stats._haversine_distance(coord1, coord2)
 
             if total_dist > dist:
                 return
             
             for neighbor in adjacency_list[nodes[-1]]:
-                if neighbor not in visited:
+                if (neighbor not in features) and (neighbor in self.abstract_nodes):
+                    return
+                elif neighbor not in visited:
                     visited.add(neighbor)
                     if  (neighbor in features) and (neighbor != nodes[0]):
                         for k in nodes:
@@ -117,5 +124,6 @@ class Graph():
         for feature in features:
             dfs([feature],features)
     
-        return saved"""
+        return saved
+
 
--- algo_graph.py
+++ algo_graph.py
@@ -4,131 +4,15 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import networkx as nx 
 from itertools import combinations
-
-
-
-
-abstract_nodes = ['A', 'B', 'C']
-nodes = [1, 2, 3, 4, 5, 6, 7]
-edges = [('A', 1), ('B', 2), ('B', 3), (1,5), (5,3)]
-
-
-def make_graph(nodes, edges):
-    g = nx.Graph()
-    g.add_nodes_from(nodes)
-    g.add_edges_from(edges)
-    return g
-
-def edges_to_adjacency_list_undirected(abstract_nodes, nodes, edges):
-    adjacency_list = {node: [] for node in abstract_nodes + nodes}
-
-    for edge in edges:
-        source, target = edge
-        adjacency_list[source].append(target)
-        adjacency_list[target].append(source)  
-
-    return adjacency_list
-
-def sub_graph(abstract_nodes, nodes, edges):
-    keep_edges = [edge for edge in edges if (edge[0] in abstract_nodes)]
-    keep_nodes = [edge[1] for edge in keep_edges]+[k for k in abstract_nodes]
-    return keep_nodes, keep_edges
-
-def test_connected(nodes, edges):
-    g = nx.Graph()
-    g.add_nodes_from(nodes)
-    g.add_edges_from(edges)
-    return nx.is_connected(g)
-
-def fully_connected(abstract_nodes, nodes, edges):
-    unconnected = []
-    connected = []
-    for i,j in combinations(abstract_nodes, 2):
-        keep_nodes, keep_edges = sub_graph([i,j], nodes, edges)
-        if test_connected(keep_nodes, keep_edges):
-            connected.append((i,j))
-        else :
-            unconnected.append((i,j))
-    return unconnected, connected
-
-def draw(G, color="red"):
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=500, node_color=color, font_size=10)
-    plt.show()
-
-def get_neighbors_at_depth(abstract_nodes, adjacency_list, features, depth):
-    visited = set()
-    saved = set()
-
-    def dfs(nodes, features):
-        print(nodes)
-        if len(nodes) > depth:
-            return
-        
-        for neighbor in adjacency_list[nodes[-1]]:
-            if (neighbor not in features) and (neighbor in abstract_nodes):
-                return
-            if neighbor not in visited:
-                visited.add(neighbor)
-                if  (neighbor in features) and (neighbor != nodes[0]):
-                    for k in nodes:
-                        if k not in features:
-                            saved.add(k)
-                dfs([k for k in nodes]+[neighbor], features)
-
-    for feature in features:
-        dfs([feature],features)
-  
-    return saved
-
-def distance(a,b):
-    return
-
-def get_neighbors_at_distance(adjacency_list, features, dist):
-    visited = set()
-    saved = set()
-
-    def dfs(nodes, features):
-        total_dist = 0
-        for i in range(1,len(nodes)-1):
-            total_dist+=distance(i,i+1)
-
-        if total_dist > dist:
-            return
-        
-        for neighbor in adjacency_list[nodes[-1]]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                if  (neighbor in features) and (neighbor != nodes[0]):
-                    for k in nodes:
-                        if k not in features:
-                            saved.add(k)
-                dfs([k for k in nodes]+[neighbor], features)
-
-    for feature in features:
-        dfs([feature],features)
-  
-    return saved
-
-
-
+from Graph import Graph
 
 
 if __name__ == "__main__":
 
-    stats = Stats(N=22500, folder='data')
-    abstract_nodes = stats.unique_key_value_pairs()
-    nodes = stats.nodes()
-    idx_to_xy, xy_to_idx = stats.mapping_dictionaries(nodes)
-    edges = stats.edges()
+    G = Graph(22500, "data")
+    G.init_nodes_and_edges(N=10)
 
-    abstract_nodes = list(abstract_nodes)
-    formatted_nodes = [xy_to_idx[node] for node in nodes]
-    formatted_edges = stats.edges_formatting(edges, xy_to_idx)
-    distance_edges = stats.distance_edges(formatted_nodes, N=10)
-
-
-    adgency_list = edges_to_adjacency_list_undirected(abstract_nodes,formatted_nodes,formatted_edges+distance_edges)
-    print(get_neighbors_at_depth(abstract_nodes, adgency_list, abstract_nodes[:4], 15))
+    adgency_list = G.edges_to_adjacency_list_undirected()
+    print(G.get_neighbors_at_depth(adgency_list, G.abstract_nodes[:4], 20))
 
 
